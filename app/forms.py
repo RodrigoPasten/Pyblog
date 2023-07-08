21372630dac5f9875eef4f1e5a9de254fa6f7bfb
rpasten <rodrigo.pasten.c@gmail.com>
2023-07-08 14:26:15 -0400

    add comment system (se agregan comentarios)

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,13 @@ from app.models import Comments
 
 
 class CommentForm(forms.ModelForm):
-    course = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Curso (opcional)'}))
-    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Tu nombre:'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email:'}))
     class Meta:
         model = Comments
         fields = {'content', 'email', 'name', 'course'}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['content'].widget.attrs['placeholder'] = 'cuenta que onda, que opinas de este post *'
+        self.fields['name'].widget.attrs['placeholder'] = "Tu nombre, amigo(a) *"
+        self.fields['email'].widget.attrs['placeholder'] = "Tu correo: *"
+        self.fields['course'].widget.attrs['placeholder'] = "Curso: (si es que quieres) "
